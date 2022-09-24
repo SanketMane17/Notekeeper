0963e5dc590a9e5fe8158a4d47f8a4a92b44ba17
Sanket <msanket450@gmail.com>
2022-09-24 12:09:30 +0530

    small change

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 function Count({ count }) {
-    return (
-        <div className="count">
-            <h4>{count} notes in Database</h4>
-        </div>
-    );
+
+    if (count > 0) {
+        if (count == 1) {
+            return (
+                <div className="count">
+                    <h4>{count} note in Database</h4>
+                </div>
+            )
+        }
+        return (
+            <div className="count">
+                <h4>{count} notes in Database</h4>
+            </div>
+        )
+    }
 }
 
 export default Count;
